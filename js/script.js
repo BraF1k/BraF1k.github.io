cf9fb45bb202375c0b4a74d8d3d050efd46a76eb
sergey <sergeyburiak44@gmail.com>
2018-10-22 21:44:15 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -10,3 +10,24 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+  window.onload = function () {
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
+};
