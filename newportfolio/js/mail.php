6b541e14c63e4fefb4ebfc421b176fab328ca41d
Serhii Buriak <sergeyburiak44@gmail.com>
2019-08-09 15:17:42 +0300

    commit

--- newportfolio/js/mail.php
+++ newportfolio/js/mail.php
@@ -1,43 +1,24 @@
 <?php 
-
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-$name = $_POST['user_name'];
-$surname = $_POST['surname'];
-$topic = $_POST['topic'];
-$email = $_POST['email'];
-$textarea = $_POST['textarea'];
-
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'sergeyburiak55@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'SErge261998'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('sergeyburiak55@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('sergeyburiak55@gmail.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка с тестового сайта';
-$mail->Body    = '' .$name. ' оставил заявку, его почта ' .$email. '<br><s>Тема письма: </s>' .$topic. '<br>' .$textarea;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
+if (!empty($_POST['name']) AND !empty($_POST['email']) AND !empty($_POST['topic'])) 
+{
+    $headers = 'From: Буряк Сергей\r\n' .
+                'Reply-To: sergeyburiak55@gmail.com\r\n' .
+                'X-Mailer: PHP/' . phpversion();
+         
+    $theme = "Новое сообщение с сайта";             
+             
+    $letter = "Данные сообщения:";
+    $letter .="\n\n";
+    $letter .="Имя: ".$_POST['name'];
+    $letter .="\nEmail: ".$_POST['email'];
+    $letter .="\nСообщение: ".$_POST['textarea'];
+    
+    if (mail('sergeyburiak55@gmail.com', $theme, $letter, $headers)){
+      header("Location: /testform/thank-you.html");
+    } else {
+      header("Location: /testform");
+    }  
+              
 } else {
-    header('location: thank-you.html');
+  header("Location: /testform");
 }
-?>
